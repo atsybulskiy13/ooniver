b1cc31f8924530c7a5818283baf2fc01e01e048e
Alex Tsybulskiy <atsybulskiy@Alexs-MacBook-Air.local>
2023-05-02 19:19:56 +0300

    First homework

---
+++ temporary_file.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+second_name = input("Enter your secondname: ")
+work_place = input("Enter your work place: ")
+age = input("Enter your work age: ")
+
+print(work_place, "\n", "----------", "\n", second_name, " ", name, ", ", age, sep="")
