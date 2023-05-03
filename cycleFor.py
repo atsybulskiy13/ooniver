9916cb85e8b2c4da41b740febda0a76d28d74c07
Alex Tsybulskiy <atsybulskiy@Alexs-MacBook-Air.local>
2023-05-03 21:54:20 +0300

    Class 4

---
+++ cycleFor.py
@@ -0,0 +1,14 @@
+number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for i in number_list:
+    print(i, end=" ")
+
+print()
+
+number_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+for i in number_tuple:
+    print(i, end=" ")
+
+print()
+
---
+++ numbers.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if not a % 2:
+    print(a)
+if not b % 2:
+    print(b)
+if a % 2 and b % 2:
+    print("There is no aven numbers!")
+
+
