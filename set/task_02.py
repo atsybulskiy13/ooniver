a73669bd27646ac6b0dcdb78b5971662ee50d270
Alex Tsybulskiy <atsybulskiy@Alexs-MacBook-Air.local>
2023-05-17 13:07:20 +0300

    Fixed issues with task_02 in set

--- set/task_02.py
+++ set/task_02.py
@@ -18,6 +18,12 @@ second_list_unics = set(second_list).difference(set(first_list))
 
 unics = set(first_list).intersection(set(second_list))
 
-print(f'Unic numbers from first list: {first_list_unics}')
-print(f'Unic numbers from second list: {second_list_unics}')
+different_elements = list(first_list_unics)
+
+for i in second_list_unics:
+    different_elements.append(i)
+
+print(f'Numbers only from first list: {first_list_unics}')
+print(f'Numbers only from second list: {second_list_unics}')
 print(f'Numbers from both lists: {unics}')
+print(f'Different numbers from both lists: {different_elements}')
