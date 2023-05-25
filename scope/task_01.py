c3175d636d5dd777c751ba08e7640aaf9bdb7b51
Alex Tsybulskiy <atsybulskiy@Alexs-MacBook-Air.local>
2023-05-25 08:26:50 +0300

    Scope & task_04 from homework

---
+++ scope/task_01.py
@@ -0,0 +1,16 @@
+goal_1 = "mitsubishi"
+goal_2 = "mersedes"
+name = "Misha"
+
+
+def print_first_goal():
+    name1 = "Sasha"
+    print(f'{name1} want to buy a car - {goal_1}')
+
+
+def print_second_goal():
+    print(f'{name} want to buy a car - {goal_2}')
+
+
+print_first_goal()
+print_second_goal()
