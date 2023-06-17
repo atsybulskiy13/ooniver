892906ab2531a57d93c1f8c8dca22d32034bea99
Alex Tsybulskiy <alextsybulskiy@Alexs-MacBook-Pro.local>
2023-06-17 20:03:46 +0300

    ooniver course test fixes

--- ooniver_course_test/task_03.py
+++ ooniver_course_test/task_03.py
@@ -1,13 +1,11 @@
 number = int(input('Enter your number: '))
 
-factorial = 1
 
-if number == 0:
-    print(f'Factorial {number} = {factorial}')
-elif number < 0:
-    print('There is no factorial for number < 0')
-else:
-    for i in range(number + 1):
-        if i > 0:
-            factorial *= i
-    print(f'Factorial {number} = {factorial}')
+def factorial(number):
+    if number == 0 or number == 1:
+        return 1
+    else:
+        return number * factorial(number - 1)
+
+
+print(f'Factorial {number} = {factorial(number)}')
--- ooniver_course_test/task_04.py
+++ ooniver_course_test/task_04.py
@@ -4,17 +4,17 @@ lenth = int(input('Enter lenth: '))
 
 random_list = []
 
-min_number = 100
-max_number = 0
-
 for i in range(lenth):
     random_num = randint(0, 100)
     random_list.append(random_num)
-    if random_num > max_number:
-        max_number = random_num
-    if random_num < min_number:
-        min_number = random_num
 
+max_number = min_number = random_list[0]
+
+for number in random_list:
+    if number > max_number:
+        max_number = number
+    if number < min_number:
+        min_number = number
 
 my_dict = {'min': min_number, 'max': max_number}
 
