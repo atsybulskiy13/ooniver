1d20521091deefa9955b2acf7c8319a918b035ad
Alex Tsybulskiy <alextsybulskiy@Alexs-MacBook-Pro.local>
2023-08-21 10:56:33 +0300

    decorators fixes

--- functional_programming/decorators_hw.py
+++ functional_programming/decorators_hw.py
@@ -1,9 +1,8 @@
 def second_elem_to_string_decorator(func):
     def wrapper(x):
-        result = func(x)
+        new_list = [str(x[i]) if i % 2 else x[i] for i in range(len(x))]
 
-        new_list = [str(result[i]) if i % 2 else result[i] for i in range(len(result))]
-        return new_list
+        return func(new_list)
 
     return wrapper
 
@@ -16,19 +15,17 @@ def second_elem_to_string(number_list):
 number_list = [x for x in range(10)]
 print(number_list)
 
-
 new_list = second_elem_to_string(number_list)
 print(new_list)
 
 
 def list_reverse_decorator(func):
     def wrapper(*args):
-        args = func(*args)
         new_args = []
         for i in args:
             new_args.insert(0, i)
 
-        return tuple(new_args)
+        return func(*new_args)
 
     return wrapper
 
