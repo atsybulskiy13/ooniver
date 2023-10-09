8b3421bdb4090fa7cab470a83da5bb56cfbe1cee
Alex Tsybulskiy <alextsybulskiy@Alexs-MacBook-Pro.local>
2023-10-09 13:25:25 +0300

    django first class

--- my_django/mysite/views.py
+++ my_django/mysite/views.py
@@ -1,8 +1,9 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
+import json
 from datetime import datetime
 
-from my_django.mysite.utils import get_simple_numbers_in_number
+from my_django.mysite.utils import get_simple_numbers_in_number, if_palindrom
 
 
 # Create your views here.
@@ -22,3 +23,52 @@ def simple_numbers(request, number):
     simple_numbers = get_simple_numbers_in_number(number)
     response = HttpResponse(f'All simple numbers in {number}: {simple_numbers}')
     return response
+
+
+# def admin_page(request):
+#     return HttpResponse(f'<h1>Вы вошли как Админ</h1>')
+
+
+def guest_page(request, role):
+    return HttpResponse(f'<h1>Вы вошли как {role}<h1>')
+
+
+def check_role(request, role):
+    if role == 'admin':
+        return redirect('admin-page')
+    else:
+        return redirect('guest-page', role=role)
+
+
+def palindrome(request, word):
+    if if_palindrom(word):
+        return HttpResponse(f'Half word: {word[:len(word) // 2]}')
+    else:
+        return redirect('main-page')
+
+
+def check_view(request: HttpRequest):
+    if request.method == 'GET':
+        page_number = request.GET.get('number')
+        return redirect('guest-page', role=page_number)
+
+    return redirect('admin-page')
+
+
+def book_booking(request: HttpRequest):
+    if request.method == 'GET':
+        return render(
+            request=request,
+            template_name='book_booking.html'
+        )
+
+    user_data = {
+        'book_name': request.POST.get('book_name'),
+        'person_name': request.POST.get('person_name'),
+        'period': request.POST.get('period')
+    }
+
+    with open('user.txt', 'w') as user_file:
+        user_file.write(json.dumps(user_data))
+
+    return HttpResponse(status=200)
